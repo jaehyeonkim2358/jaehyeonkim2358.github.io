ec5eda6a948c803edea7159ddbb2f66f8c2f157c
jaehyeon-crema <jaehyeon.kim@cre.ma>
2024-01-11 23:18:38 +0900

    change pages-deploy.yml

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-1704977296';
+const cacheName = 'chirpy-1704982516';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
